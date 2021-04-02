fa39d7a6989a3a20f61292d33db0aaaf12907dcc
Admin <patilsharad2000@gmail.co>
2021-04-02 08:47:20 +0530

    Stereotype Annotation using component

---
+++ src/main/java/com/stereotypeannotation/Student.java
@@ -0,0 +1,34 @@
+package com.stereotypeannotation;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Student {
+	@Value("Sharad Patil")
+	private String name;
+	
+	@Value("Dhule")
+	private String city;
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getCity() {
+		return city;
+	}
+	
+	public void setCity(String city) {
+		this.city = city;
+	}
+	
+	@Override
+	public String toString() {
+		return "Student [name=" + name + ", city=" + city + "]";
+	}
+}
